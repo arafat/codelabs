fe1911111a5e779bd76aa513adfd77eae34a315b
Arafat Eajaz <arafat.eajaz@gmail.com>
2020-05-14 22:15:13 +0530

    .

--- coursera/cs-programming-with-a-purpose/week1/CMYKtoRGB.java
+++ coursera/cs-programming-with-a-purpose/week1/CMYKtoRGB.java
@@ -1,12 +1,12 @@
 public class CMYKtoRGB {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         double c = Double.parseDouble(args[0]);
         double m = Double.parseDouble(args[1]);
         double y = Double.parseDouble(args[2]);
         double k = Double.parseDouble(args[3]);
         double white = 1-k;
-        System.out.println("red = " + (int)(255*white*(1-c)));
-        System.out.println("green = " + (int)(255*white*(1-m)));
-        System.out.println("red = " + (int)(255*white*(1-y)));
+        System.out.println("red = " + (int) (255*white*(1-c)));
+        System.out.println("green = " + (int) (255*white*(1-m)));
+        System.out.println("red = " + (int) (255*white*(1-y)));
     }
 }
--- coursera/cs-programming-with-a-purpose/week1/RightTriangle.java
+++ coursera/cs-programming-with-a-purpose/week1/RightTriangle.java
@@ -1,23 +1,8 @@
 public class RightTriangle {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
-        if(a<0 || b<0 || c<0) {
-            System.out.println(false);
-            return;
-        }
-        if (a*a + b*b == c*c) {
-            System.out.println(true);
-            return;
-        }
-        if (b*b + c*c == a*a) {
-            System.out.println(true);
-            return;
-        }
-        if (a*a + c*c == b*b) {
-            System.out.println(true);
-            return;
-        }
+        System.out.println((a>=0 && b>=0 && c>=0) && ((a*a + b*b == c*c) || (b*b + c*c == a*a) || (a*a + c*c == b*b)));
     }
 }
